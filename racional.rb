c44881e605321dbd6701ed08149d4456d12754f0
alu0100596113 <alu0100596113@ull.edu.es>
2013-10-17 16:20:49 +0100

    Constructor implementado

--- racional.rb
+++ racional.rb
@@ -3,4 +3,19 @@
 require "./gcd.rb"
 
 class Fraccion
+	attr_reader :a, :b
+	
+	def initialize(a, b)
+		mcd = gcd(a, b) 	
+    @a = a/mcd
+	    if b != 0
+	      @b = b/mcd
+	    else
+	      raise ArgumentError , 'ERROR: El denominador no puede ser 0.'
+	    end
+	end
 end
+
+#A = Fraccion.new(2,3)
+#puts A
+
